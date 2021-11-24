70760100bdf61af240cb1b205c2e732863bdf722
Scharif47 <scharif47@outlook.de>
2021-11-24 20:52:30 +0100

    dynamic background added

--- src/Todo.js
+++ src/Todo.js
@@ -47,22 +47,34 @@ function Todo() {
   return (
     <div className="App mt-8">
       <h1 className="text-6xl mb-3">Tasks</h1>
-      <h2 className="text-3xl mb-3">
-        You have {tasks.length} {tasks.length === 1 ? "task" : "tasks"}
-      </h2>
-      <div className="p-2 shadow-lg pb-8 flex justify-center">
-        <input
-          className="border-2 text-xl border-red-800 w-96 h-12 px-1 bg-green-200"
-          value={input}
-          onKeyPress={handleKeytoDo}
-          onChange={onInputChange}
-        />
-        <button
-          onClick={handleTodo}
-          className="border-2 border-red-800 px-8 bg-green-600 text-2xl font-bold"
-        >
-          +
-        </button>
+      <div
+        className={`p-8 shadow-lg pb-8 ${
+          tasks.length < 1
+            ? "bg-white"
+            : tasks.length < 4
+            ? "bg-green-400"
+            : tasks.length < 6
+            ? "bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"
+            : "bg-red-800"
+        }`}
+      >
+        <h2 className="text-3xl mb-3">
+          You have {tasks.length} {tasks.length === 1 ? "task" : "tasks"}
+        </h2>
+        <div className=" flex justify-center">
+          <input
+            className="border-2 text-xl border-red-800 w-96 h-12 px-1 bg-green-200"
+            value={input}
+            onKeyPress={handleKeytoDo}
+            onChange={onInputChange}
+          />
+          <button
+            onClick={handleTodo}
+            className="border-2 border-red-800 px-8 bg-green-600 text-2xl font-bold"
+          >
+            +
+          </button>
+        </div>
       </div>
 
       <div
